ae473d0476afbb3c6d27d01b27a6e0e23c9fdacb
acquahsamuel <acquahsamuel1996@gmail.com>
2021-04-15 14:42:18 +0000

    server.js

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({path: './config.env'});
 
 // Connect to database
 connectDB();
@@ -55,7 +55,7 @@ app.use(xss());
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 mins
-  max: 100
+  max: 100,
 });
 app.use(limiter);
 
@@ -77,18 +77,18 @@ app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-  // Close server & exit process
-  // server.close(() => process.exit(1));
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
 });
+
+//@desc  Handle unhandled Rejection
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION Shutting down');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 
